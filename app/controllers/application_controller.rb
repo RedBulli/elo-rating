3e348df00585c577559704e1f76938113c672e0a
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2016-02-28 21:26:10 +0200

    Use basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :verify_logged_in
+
+  private
+
+  def verify_logged_in
+    authenticate_or_request_with_http_basic do |nick, pass|
+      nick == (ENV['USERNAME'] || 'user') and pass == (ENV['PASSWORD'] || 'password')
+    end
+  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ActiveRecord::Base
   belongs_to :elo
   after_create :create_elo
-  validates :name, length: { minimum: 1 }, uniqueness: true
+  validates :name, length: { minimum: 1 }, uniqueness: { case_sensitive: false }
 end
