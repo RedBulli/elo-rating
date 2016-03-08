d5f6e81daded90058c5e668fb208f01d156602bc
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2016-03-08 21:19:42 +0200

    Allow player merging

--- app/models/player.rb
+++ app/models/player.rb
@@ -10,4 +10,22 @@ class Player < ActiveRecord::Base
   def self.find_or_create_by_name(name)
     where('lower(name) = ?', name.downcase).first || create(name: name)
   end
+
+  def merge_player(player)
+    if frames_against_with(player).count > 0
+      raise 'Merging players who have played against each other is not allowed'
+    end
+    ActiveRecord::Base.transaction do
+      Frame.where(winner: player).update_all(winner_id: self.id)
+      Elo.where(player: player).update_all(player_id: self.id)
+    end
+  end
+
+  private
+
+  def frames_against_with(player)
+     Frame
+      .joins(player1_elo: :player, player2_elo: :player)
+      .where('(players.id = ? OR players.id = ?) AND (players_elos.id = ? OR players_elos.id = ?)', id, player.id, id, player.id)
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -5,4 +5,27 @@ RSpec.describe Player, type: :model do
     player = Player.create!(name: 'Sampo')
     expect(player.elo.player).to eql(player)
   end
+
+  describe '#merge_player' do
+    it 'merges the player with all the elos and frames' do
+      player = Player.create!(name: 'Sampo')
+      alter_ego = Player.create!(name: 'Bad Sampo')
+      opponent = Player.create!(name: 'Oskari')
+      Frame.create!(player1_elo: player.elo, player2_elo: opponent.elo, winner: player)
+      Frame.create!(player1_elo: alter_ego.elo, player2_elo: opponent.elo, winner: opponent)
+      Frame.create!(player1_elo: alter_ego.elo, player2_elo: opponent.elo, winner: alter_ego)
+      Frame.create!(player1_elo: player.elo, player2_elo: opponent.elo, winner: player)
+      player.merge_player(alter_ego)
+      expect(Frame.where(winner: player).count).to eql(3)
+    end
+
+    it "doesn't allow merge if the players have played against each other" do
+      player = Player.create!(name: 'Sampo')
+      alter_ego = Player.create!(name: 'Bad Sampo')
+      Frame.create!(player1_elo: player.elo, player2_elo: alter_ego.elo, winner: player)
+      expect {
+        player.merge_player(alter_ego)
+      }.to raise_error(RuntimeError)
+    end
+  end
 end
