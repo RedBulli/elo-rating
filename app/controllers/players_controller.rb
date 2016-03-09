32db458183fd5e6897b37ae755aefe2fdc69fb9f
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2016-03-09 19:28:25 +0200

    Speed up index showing

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,21 +1,16 @@
 class PlayersController < ApplicationController
   def index
-    @players = find_players_sorted_by_elo
-    @frames = Frame.all.order(created_at: :desc).map do |frame|
+    @players = Player.includes(:elo).all.order('elos.rating DESC')
+    last_frame = Frame.order('id DESC').first
+    @frames = Frame.includes(:player1_elo, :player2_elo).all.order(created_at: :desc).map do |frame|
       {
-        breaker_is_winner: frame.player1_elo.player == frame.winner,
-        player1: frame.player1_elo.player,
-        player2: frame.player2_elo.player,
+        breaker_is_winner: frame.player1 == frame.winner,
+        player1: frame.player1,
+        player2: frame.player2,
         created_at: frame.created_at,
-        deletable: frame.deletable?,
+        deletable: frame == last_frame,
         model: frame
       }
     end
-  end
-
-  private
-
-  def find_players_sorted_by_elo
-    Player.includes(:elo).all.sort_by { |player| -player.elo.rating }
   end
 end
