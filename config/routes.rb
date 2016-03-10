330121d60d6e774e2bbf208be30834c53c2df559
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2016-03-10 21:43:09 +0200

    Put sidekiq ui behid password

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
   root 'players#index'
@@ -8,5 +7,8 @@ Rails.application.routes.draw do
   resources :frames, only: [:create, :destroy]
 
   require 'sidekiq/web'
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
+  end if Rails.env.production?
   mount Sidekiq::Web => '/sidekiq'
 end
